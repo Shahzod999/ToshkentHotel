e48959c73246fce76e41f251e7d819e9692efdaa
Shahzod <sibragimov999@gmail.com>
2024-11-06 20:13:16 +0500

    access

--- frontend/src/app/api/apiSlice.ts
+++ frontend/src/app/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/` });
+const baseQuery = fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/`, credentials: "include" });
 
 export const apiSlice = createApi({
   baseQuery,
