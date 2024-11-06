57dc77bae9621f2226720eb6c97f3b73cdadad6c
Shahzod <sibragimov999@gmail.com>
2024-11-06 19:21:54 +0500

    corss

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://toshkent-hotel.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
